e30c73bfceef689d9bb256e56d26eb471ceed28a
hlhdaiaaii <hdaicenter@gmail.com>
2020-12-03 15:04:55 +0700

    add public and views folder

--- EzTest/app.js
+++ EzTest/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
