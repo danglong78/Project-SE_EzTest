ed1ddbc9d446ff7cc41cf37a07d283cdaada2448
longd <18127011@student.hcmus.edu.vn>
2020-12-15 02:06:03 +0700

    Add Edit QUiz

--- EzTest/src/quiz/routes.js
+++ EzTest/src/quiz/routes.js
@@ -12,10 +12,10 @@ quizRouter.get('/add', function (req, res) {
     res.render('quiz/add');
 });
 quizRouter.post('/add', async function (req, res) {
-    let temp = JSON.parse(JSON.stringify(req.body));
-    console.log(temp);
+    // let temp = JSON.parse((req.body));
+    let temp=req.body;
 
-    let Quiz = new quizModel({
+    let quiz = new quizModel({
         uploader: req.user._id,
         title: temp.title,
         count_taker: 0,
@@ -26,9 +26,9 @@ quizRouter.post('/add', async function (req, res) {
         questions: temp.questionList,
         right_ans_count: 0
     })
+     await quiz.save();
     console.log("Success");
     res.redirect("/quiz")
-    await Quiz.save();
 
 });
 quizRouter.post('/delete', async function (req, res) {
@@ -39,9 +39,31 @@ quizRouter.post('/delete', async function (req, res) {
 });
 quizRouter.get('/edit', async function (req, res) {
     let id = req.query.id;
+
     let quiz = await quizModel.find({ _id: id });
-    console.log(quiz);
-    res.render("quiz/edit", { quiz: quiz });
+    console.log(quiz[0].questions);
+    res.render("quiz/edit", { quiz: quiz[0] });
+});
+quizRouter.post('/edit', async function (req, res) {
+    // let temp = JSON.parse((req.body));
+    let temp=req.body;
+
+    // let quiz = new quizModel({
+    //     _id: temp.id,
+    //     uploader: req.user._id,
+    //     title: temp.title,
+    //     count_taker: 0,
+    //     rate: [],
+    //     checked: false,
+    //     label: [],
+    //     description: "",
+    //     questions: temp.questionList,
+    //     right_ans_count: 0
+    // })
+    await quizModel.update({_id:temp.id},{title: temp.title,questions: temp.questionList});
+    console.log("Success");
+    res.redirect("/quiz")
+
 });
 
 module.exports = quizRouter;
